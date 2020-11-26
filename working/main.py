c4d6b8be2dea1d227f59546d0c2db010be666c0e
dt-woods <twdavis@wm.edu>
2020-11-25 21:41:45 -0500

    create OutClass; addresses #8

--- working/main.py
+++ working/main.py
@@ -20,6 +20,7 @@ import copy
 import os
 
 from superf.NoteMaster import NoteMaster
+from superf.OutClass import OutClass
 from superf.ProcSound import ProcSound
 from superf.Plotting import Plotting
 from superf.utilities import get_note
@@ -41,20 +42,25 @@ if __name__ == "__main__":
     args = p.parse_args()
 
     if not os.path.isfile(args.file):
-        filename = input("Enter path to music file: ")
+        file_path = input("Enter path to music file: ")
     else:
-        filename = args.file
+        file_path = args.file
+
+    # Read the file name from input file path
+    file_name = os.path.basename(file_path)
 
     # Initialze plotting class
     my_plot = Plotting()
-    my_plot.setup(os.path.basename(filename))
+    my_plot.setup(file_name)
 
-    # Initialize notemaster class
+    # Initialize notemaster and output data class
     my_note = NoteMaster()
+    my_out = OutClass(file_name)
 
-    # Read audio file and split the data into frames
-    my_note.read_audio(filename)
+    # Read audio file, split the data into frames, and setup output vals
+    my_note.read_audio(file_path)
     my_note.split_frames()
+    my_out.setup_with_note(my_note)
 
     # Set the bottom plot in plotting:
     my_plot.plot(my_note.data, None, 3)
@@ -69,10 +75,12 @@ if __name__ == "__main__":
         frame = framed_data.pop(0) #takes the first frame
         note_freq = my_proc.find_all_peaks(my_plot, frame, my_note.fs) #gets all of the frequencies in the selected sound frame
         note = get_note(note_freq) #determines the note associated with the frame
-        print(str(i) + 'note: ' + str(note)) #print the result
+        my_out.add_peaks(i, note_freq)
+        #print(str(i) + 'note: ' + str(note)) #print the result
         # Update plotting cavas
         my_plot.patch(i, my_note.height, my_note.ymin, frame)
         my_plot.fig.canvas.draw()
 
     input('Press Enter to continue....')
     my_plot.close()
+    my_out.print_json()
---
+++ working/superf/OutClass.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+#
+# OutClass.py
+#
+# VERSION 0.0.0
+#
+# LAST EDIT: 2020-11-25
+#
+# This module is a part of the Superf package.
+
+##############################################################################
+# REQUIRED MODULES
+##############################################################################
+import json
+
+from .utilities import get_note
+
+##############################################################################
+# CLASSES
+##############################################################################
+class OutClass(object):
+    """
+    Name:     OutClass
+    Features: Class organizer for writing to output format(s)
+    """
+    # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    # Class Parameters
+    # ////////////////////////////////////////////////////////////////////////
+    filename = ""      # basename for a given audio file
+    samplerate = 0     # sampling rate of the audio file (Hz)
+    framecount = 0     # number of frames used to process audio file
+    framesize = 0      # number of samples per frame
+    samplecount = 0    # number of samples in an audio file
+    data = dict()      # empty data dictionary
+
+    # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    # Class Initialization
+    # ////////////////////////////////////////////////////////////////////////
+    def __init__(self, file_name=None):
+        """
+        Name:     OutClass.__init__
+        Inputs:   None
+        Features: Initializes the OutClass class
+        """
+        if file_name is not None:
+            self.filename = file_name
+
+    # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    # Class Function Definitions
+    # ////////////////////////////////////////////////////////////////////////
+    def print_json(self):
+        """
+        TODO: build the data dictionary + get the note
+        """
+        self.json = {
+            "properties": {
+                "filename": self.filename,
+                "samplerate": self.samplerate,
+                "framecount": self.framecount,
+                "framesize": self.framesize
+            },
+            "data": [
+                {
+                    "index": 0,
+                    "peaks": [],
+                    "note": "Am"
+                }
+            ]
+        }
+        js_obj = json.dumps(self.json, indent = 2)
+        print(js_obj)
+
+
+    def add_peaks(self, frame_index, peak_list):
+        """Add peak data to data dictionary"""
+        if frame_index in self.data.keys():
+            print("Data index, {}, already exists!".format(frame_index))
+        else:
+            self.data[frame_index] = peak_list
+
+
+    def setup_with_note(self, note_class):
+        """
+        Name:     OutClass.setup_with_note
+        Inputs:   NoteMaster
+        Outputs:  None
+        Features: Sets class values using the NoteMaster class
+        TODO:     _ update with Tim's framesize in NoteMaster
+        """
+        self.samplerate = note_class.sample_rate
+        self.framecount = note_class.num_frames
+        self.samplecount = note_class.num_samples
+        self.framesize = note_class.frame_size
